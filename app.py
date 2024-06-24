8d3d1d1d2388da841d558cd7b9fa0d8b9d33f597
kjaehnig <jaehnig.k@gmail.com>
2024-06-24 18:33:32 -0500

    added image flips

--- app.py
+++ app.py
@@ -114,8 +114,8 @@ use_modified_img = st.sidebar.checkbox("Use modified Image")
 alpha_val = st.sidebar.slider("Alpha (contrast)", min_value=0.0, max_value=3.0, value=1.0, step=0.1)
 beta_val = st.sidebar.slider("Beta (brightness)", min_value=0, max_value=100, value=0, step=1)
 
-flip_image_h = st.sidebar.selectbox("Flip Image (horizontal)")
-flip_image_v = st.sidebar.selectbox("Flip Image (vertical)")
+flip_image_h = st.sidebar.checkbox("Flip Image (horizontal)")
+flip_image_v = st.sidebar.checkbox("Flip Image (vertical)")
 
 predict_image = st.sidebar.button("Predict!")
 
