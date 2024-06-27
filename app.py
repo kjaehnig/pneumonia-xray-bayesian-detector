4bd9a55a08a85d274fe9d8f87af020d980fc779a
Karl Jaehnig <jaehnig.k@gmail.com>
2024-06-27 17:07:33 -0400

    added medical warning

--- app.py
+++ app.py
@@ -78,11 +78,11 @@ with st.expander("Description"):
     reliable pneumonia detection from chest X-rays, improving treatment outcomes and resource allocation in healthcare.
     """)
 
-non_medical_warning = r'$\textcolor{red}{\
-        \textsf{\
-            \small This app is intended to be solely demonstrative and SHOULD \
-            NOT be used for any medical or diagnostic purposes.'
-st.write(non_medical_warning)
+non_medical_warning = """
+    :red [This app is entirely demonstrative and **SHOULD NOT** be used for any medical or diagnostic
+    purposes.]
+    """
+st.markdown(non_medical_warning)
 
 st.sidebar.title("Settings")
 
