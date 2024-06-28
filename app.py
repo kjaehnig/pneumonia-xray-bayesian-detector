ffe7f069ba848c9e73d50c37c1de1abb0f452613
Karl Jaehnig <jaehnig.k@gmail.com>
2024-06-27 22:29:29 -0400

    debugging

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def make_comparative_preds(model, images, n_iter):
 
 
 def make_violin_fig(true_int, predicted_probabilities, class_names):
+    predicted_probabilities = predicted_probabilities.reshape(-1,1)
     pct_2p5 = np.percentile(predicted_probabilities, 2.5, axis=0)
     pct_97p5 = np.percentile(predicted_probabilities, 97.5, axis=0)
     pct_50 = np.percentile(predicted_probabilities, 50, axis=0)
