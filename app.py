c07178df52dbac153d8538223dfb128e84dfae1a
Karl Jaehnig <jaehnig.k@gmail.com>
2024-06-27 23:11:14 -0400

    changed shot noise sttgs

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ alpha_val = st.sidebar.slider("Alpha (contrast)", min_value=0.0, max_value=3.0, 
 beta_val = st.sidebar.slider("Beta (brightness)", min_value=0, max_value=100, value=0, step=1)
 
 # shot_cols = st.sidebar.columns(2)
-shot_noise = st.sidebar.slider("Shot Noise Factor", min_value=0, max_value=1, step=0.05)
+shot_noise = st.sidebar.slider("Shot Noise Factor", min_value=0.0, max_value=1.0, step=0.05)
 # shot_switch = shot_cols[1].checkbox(" ")
 
 if selected_image_file:
